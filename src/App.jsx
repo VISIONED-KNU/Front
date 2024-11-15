1ffc40778f6d44fbc1dfebae7534f0a0602aee2a
LeeJuyeong <6leejuyeong6@gmail.com>
2024-11-16 04:30:46 +0900

    version10

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./styles.css";
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Main } from "./Main/Main";
+import { Main } from "./main";
 
 export default function App() {
   return (
